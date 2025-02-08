168ed15ab543c4cf38933de6206971c9bda761d4
Cao Qihao <caoqihao88@gmail.com>
2025-02-08 18:43:42 +0800

    班级列表模块编写完成

---
+++ grades/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path,include
+from .views import GradeListView
+
+urlpatterns = [
+    path('', GradeListView.as_view(),name='grades_list')
+]
--- grades/views.py
+++ grades/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Grade
 
 # Create your views here.
+class GradeListView(ListView):
+    model = Grade
+    template_name = 'grades/grades_list.html'
+    fields = ['grade_name', 'grade_number']
