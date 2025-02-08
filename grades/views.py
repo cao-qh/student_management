ce9733b58329fb2cf458b1dc25d41326e07d076b
Cao Qihao <caoqihao88@gmail.com>
2025-02-08 23:17:11 +0800

    学生管理系统主页完成

--- grades/views.py
+++ grades/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.views.generic import ListView
+from django.db.models import Q
 
 from .models import Grade
 
@@ -8,3 +8,15 @@ class GradeListView(ListView):
     model = Grade
     template_name = 'grades/grades_list.html'
     fields = ['grade_name', 'grade_number']
+    context_object_name = 'grades'
+    paginate_by = 1
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search = self.request.GET.get('search')
+        if search:
+            queryset = queryset.filter(
+                Q(grade_name__icontains=search) |
+                Q(grade_number__icontains=search)
+            )
+        return queryset
