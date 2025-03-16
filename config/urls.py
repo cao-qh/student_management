9c203497dd741ad02fcb39551d41f4aa91d7c563
Cao Qihao Company <caoqihao88@gmail.com>
2025-03-16 15:37:54 +0800

    未完结

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from accounts import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('grades/',include('grades.urls')),
     path('students/',include('students.urls')),
     path('teachers/',include('teachers.urls')),
-    path('scores/',include('scores.urls'))
+    path('scores/',include('scores.urls')),
+    path('login/',views.user_login,name='user_login')
 ]
