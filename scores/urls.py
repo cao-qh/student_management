7d66a3524c3857f1d625ef68639fa26f9fee0ace
Cao Qihao Company <caoqihao88@gmail.com>
2025-03-16 17:52:23 +0800

    83  6:14

--- scores/urls.py
+++ scores/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import  ScoreListView, ScoreCreateView, ScoreUpdateView, ScoreDeleteView, ScoreDeleteMultipleView, ScoreDetailView, MyScoreListView
+from .views import upload_scores, export_scores, ScoreListView, ScoreCreateView, ScoreUpdateView, ScoreDeleteView,ScoreDeleteMultipleView, ScoreDetailView, MyScoreListView
 
 urlpatterns = [
     path('', ScoreListView.as_view(), name='score_list'),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('delete/<int:pk>/', ScoreDeleteView.as_view(), name='score_delete'),
     path('delete_multiple/', ScoreDeleteMultipleView.as_view(), name='score_delete_multiple'),
     path('detail/<int:pk>/', ScoreDetailView.as_view(), name='score_detail'),
-    # path('upload_scores/', upload_scores, name="upload_scores"),
-    # path('export_scores/', export_scores, name="export_scores"),
+    path('upload_scores/', upload_scores, name="upload_scores"),
+    path('export_scores/', export_scores, name='export_scores'),
     path('my_scores', MyScoreListView.as_view(), name='my_scores'),
 ]
